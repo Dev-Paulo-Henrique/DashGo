714a6e432e205006c0a321922fb10ffcf6cfea28
Paulo Henrique <jwcfree11@gmail.com>
2022-04-14 13:51:22 -0300

    DashGo

--- src/components/Header/NotificationsNav.tsx
+++ src/components/Header/NotificationsNav.tsx
@@ -1,26 +1,11 @@
-import { Icon, HStack, Link as ChakraLink, LinkProps as ChakraLinkProps} from '@chakra-ui/react'
+import { Icon, HStack} from '@chakra-ui/react'
 import { RiNotificationLine, RiUserAddLine } from 'react-icons/ri'
-import { ElementType } from 'react'
-import { ActiveLink } from "../ActiveLink";
 
-interface IconLinkProps extends ChakraLinkProps{
-  icon: ElementType;
-  href: string;
-}
-
-export function NotificationsNav({icon, href, ...rest}:IconLinkProps) {
+export function NotificationsNav() {
   return (
     <HStack spacing={["6", "8"]} mx={["6", "8"]} pr={["6", "8"]} py="1" color="gray.300" borderRightWidth={1} borderColor="gray.700">
-          <ActiveLink href="/notifications" passHref>
-    <ChakraLink display="flex" align="center" {...rest}>
-        <Icon as={RiNotificationLine} fontSize="20" cursor={"pointer"}/>
-      </ChakraLink>
-      </ActiveLink>
-          <ActiveLink href="/users/create" passHref>
-    <ChakraLink display="flex" align="center" {...rest}>
-        <Icon as={RiUserAddLine} fontSize="20" cursor={"pointer"}/>
-      </ChakraLink>
-      </ActiveLink>
+          <Icon as={RiNotificationLine} fontSize="20"/>
+          <Icon as={RiUserAddLine} fontSize="20"/>
         </HStack>
   )
 }
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -61,37 +61,17 @@ const series = [
 
 export default function Dashboard() {
   return (
-    <Flex direction="column" h="100vh" overflowY="auto"
-    css={{
-      '&::-webkit-scrollbar': {
-        width: '4px',
-      },
-      '&::-webkit-scrollbar-track': {
-        width: '6px',
-      },
-      '&::-webkit-scrollbar-thumb': {
-        background: theme.colors.gray[600],
-        borderRadius: '24px',
-      },
-    }}>
+    <Flex direction="column" h="100vh">
       <Header/>
       <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
       <Sidebar/>
       <SimpleGrid flex="1" gap="4" minChildWidth="320px" align="flex-start">
         <Box p={["6", "8"]} bg="gray.800" borderRadius={8} pb="4">
-        <Text fontSize="lg" mb="4">Registros</Text>
+        <Text fontSize="lg" mb="4">Inscritos da semana</Text>
         <Chart options={options} series={series} type="area" height={160}/>
         </Box>
         <Box p={["6", "8"]} bg="gray.800" borderRadius={8}  pb="4">
-        <Text fontSize="lg" mb="4">Uso da Wendy</Text>
-        <Chart options={options} series={series} type="area" height={160}/>
-        </Box>
-        <Box p={["6", "8"]} bg="gray.800" borderRadius={8}  pb="4">
-        <Text fontSize="lg" mb="4">Pagamentos</Text>
-        <Chart options={options} series={series} type="area" height={160}/>
-        </Box>
-        <Box p={["6", "8"]} bg="gray.800" borderRadius={8}  pb="4">
-        <Text fontSize="lg" mb="4">Clientes</Text>
+        <Text fontSize="lg" mb="4">Taxa de abertura</Text>
         <Chart options={options} series={series} type="area" height={160}/>
         </Box>
       </SimpleGrid>
